8a3e2b52f748921271bd8d55995964da42ecaf02
Lisa <lisa@collisa.be>
2022-06-09 22:30:17 +0200

    Added 1 to the index for the blokRelation showcase, index starting 0 for someone not IT related just looks weird

--- components/utils/Card.vue
+++ components/utils/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center p-5 space-y-5 rounded-xl border-4 border-black shadow hover:shadow-xl">
     <div class="text-4xl font-bold">
-      {{ index }}
+      {{ index + 1 }}
     </div>
     <div class="text-2xl uppercase">
       {{ name }}
